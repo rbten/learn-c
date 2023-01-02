dd9392c53c5907c0f61c9928076c7fb7c98f90cc
thinh <thinh.personal@gmail.com>
2023-01-02 23:01:14 +0700

    script

---
+++ codecademy-learn-c/1-hello-world/script.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("Simple Recipe!\n");
+
+  // Simple Recipe
+  printf("2 Cups: All Purpose Flour\n");
+  printf("1 Cups: Unsalted Butter\t(Room Temperature)");
+  printf("\n2/3 Cups: Granulated Sugar");
+}
