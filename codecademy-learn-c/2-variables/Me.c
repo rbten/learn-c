a39e8dac8e252d8f21913e56559cebe3930ac0a1
thinh <thinh.personal@gmail.com>
2023-01-04 22:35:56 +0700

    Me

---
+++ codecademy-learn-c/2-variables/Me.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int numOfBooks = 12;
+  char favLetter = 'R';
+  char favDigit = '7';
+  double costOfBooks = 100.5;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your favorite letter: %c\n", favLetter);
+  printf("Your favorite digit: %c\n", favDigit);
+  printf("Cost of all books: %f", costOfBooks);
+}
