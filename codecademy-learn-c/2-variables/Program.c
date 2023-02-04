475322c9bf828aa7e7312a4e8768687801857ea7
thinh <thinh.personal@gmail.com>
2023-02-04 22:24:51 +0700

    Program

--- codecademy-learn-c/2-variables/Program.c
+++ codecademy-learn-c/2-variables/Program.c
@@ -14,4 +14,25 @@ int main() {
   printf("Student's rank: %dst\n", studentRank);
   printf("Student's Total Fees: $%3.2f\n", studentFeeTotal);
   printf("Student's rank: %c\n", studentGradeLetter);
+
+  int numOfLoops = 10;
+
+  // Please do not modify anything below this point
+  float a = 0.1f;
+  float b = 0;
+  double x = 0.1;
+  double y = 0;
+
+  printf("At the start, our target float b is:%f\n", b);
+  printf("At the start, our target double y is:%f\n", y);
+
+// If you were curious about what this code is doing, it is looping through and adding to our variables b and y a set amount of 0.1 on each loop
+  for(int i = 0; i < numOfLoops; i++)
+  {
+    b += a;
+    y += x;
+  }
+
+  printf("At the end, our target float b is:%f\n", b);
+  printf("At the end, our target double y is:%f\n", y);
 }
