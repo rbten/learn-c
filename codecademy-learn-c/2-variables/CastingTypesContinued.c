2c02ff194d2aa41cf7848bedcea4ff1e969138b7
thinh <thinh.personal@gmail.com>
2023-02-15 22:59:17 +0700

    CastingTypesContinued

--- codecademy-learn-c/2-variables/CastingTypesContinued.c
+++ codecademy-learn-c/2-variables/CastingTypesContinued.c
@@ -8,6 +8,8 @@ int main() {
 
   // Cast here
   targetChar = (char)sourceInt;
+  targetChar = sourceDouble;
+
 
   // No need to change below here
   printf("source int %d, source double, %.2f, target %c\n", sourceInt, sourceDouble, targetChar);
