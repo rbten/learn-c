b25c7b3edbed406ae556f41af5d5beedd8b5768d
thinh <thinh.personal@gmail.com>
2023-02-27 23:48:41 +0700

    LearnModulo

--- codecademy-learn-c/3-operators/LearnModulo.c
+++ codecademy-learn-c/3-operators/LearnModulo.c
@@ -6,6 +6,8 @@ int main() {
   int daysThatPass = 1;
   int daysInWeek = 7;
 
+  endingDayOfWeek = daysThatPass% daysInWeek;
+
 
 
   printf("You started on the 1st (0) day of the week, went %d days from this, so it is now the %d day of the week\n", daysThatPass, endingDayOfWeek);
